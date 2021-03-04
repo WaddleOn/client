65c720bf9df81c9725bd0ae0e9c45d3c2597fff9
floogal <67554336+floogal@users.noreply.github.com>
2021-03-04 16:24:01 +0000

    Delete preload.js

--- preload.js
+++
@@ -1,5 +0,0 @@
-function redirect(){window.location.href = 'https://client.waddleon.fun';}
-window.addEventListener('DOMContentLoaded', () => {
-  var url = window.location.href;
-    if(url.includes('') == false){redirect();}
-  });
